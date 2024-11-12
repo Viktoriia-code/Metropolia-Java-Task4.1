b3b8d23e77306d260a1b3e9f28dc224727ba4e40
farahelb <farah.elb26@gmail.com>
2024-11-12 11:25:51 +0200

    refactored some variable and added comments

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,36 +1,37 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        System.out.println("Enter a string: ");
+        String firstInput = scanner.nextLine();
+        String changedInput = transformInput(firstInput);
 
-        String transformed = transformInput(input);
-
-        boolean isPalindrome = checkPalindrome(transformed);
+        boolean isPalindrome = isPalindrome(changedInput);
         if (isPalindrome) {
             System.out.println("The transformed input is a palindrome.");
         } else {
             System.out.println("The transformed input is not a palindrome.");
         }
+        scanner.close();
     }
 
+// change the input to lowercase and removing the "," and "!"
     public static String transformInput(String input) {
         input = input.toLowerCase();
-        input = input.replace(",", "");
-        input = input.replace("!", "");
+        input = input.replace(",", "").replace("!", "");
         return input;
     }
 
-    public static boolean checkPalindrome(String input) {
+// check if the string is a palindrome
+    public static boolean isPalindrome(String input) {
         String reversed = reverseString(input);
-        if (input.equals(reversed)) {
-            return true;
-        } else {
-            return false;
-        }
+        return input.equals(reversed);
     }
 
+    // reverse the string
     public static String reverseString(String input) {
         StringBuilder reversed = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
